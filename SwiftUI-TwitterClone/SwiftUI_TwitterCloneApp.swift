8f62dfcc9167a7c4a4911d6609ae97dd1a22b8ec
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2022-01-07 13:36:42 +0300

    Initial Commit

---
+++ SwiftUI-TwitterClone/SwiftUI_TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TwitterCloneApp.swift
+//  SwiftUI-TwitterClone
+//
+//  Created by Alexander Sobolev on 07.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
